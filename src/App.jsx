0cba513c1a13c2003de63379c3ff5dd2b092b753
BharatKhatwani <bharatkhatwani06@gmail.com>
2024-07-03 18:57:06 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+
+import './App.css'
+
+import Slide from './component/Slide.jsx'
+import React from 'react';
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    
+    <Slide/>
+   
+    </>
+  )
+}
+
+export default App
+
+
+
+
+
